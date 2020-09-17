1ee4ac9228e6b9a1b72f8c3701c13f4681d576d1
Rishabh Verma <me@rishabhverma.me>
2020-09-17 12:48:01 +0530

    Minor fixes.

--- src/auth/asyncActions.js
+++ src/auth/asyncActions.js
@@ -2,7 +2,7 @@ import { ngFetch } from '../providers/NGFetch';
 import { authSuccess } from './index';
 
 const googleAuthSuccess = (googleResponse) => async (dispatch) => {
-  const body = { idToken: googleResponse.tokenObj.id_token };
+  const body = { idToken: googleResponse.tokenObj.id_token, mode: 'web' };
   const json = await ngFetch('/users/auth/google', { method: 'POST', body });
   const { token, user } = json;
   dispatch(authSuccess({ token, user }));
